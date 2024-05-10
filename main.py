0824d14fd6725fffc5a853c8a2c5fd5203551aee
Жавохир⚜️ <144713458+maxsudovvx11@users.noreply.github.com>
2024-05-10 20:52:58 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+# Futbol kulüpleri listesi
+futbol_kulupleri = ["ochirish","ochirmaslik"]
+
+# Rastgele bir futbol kulübü seçme
+secilen_kulup = random.choice(futbol_kulupleri)
+
+# Seçilen kulübü ekrana yazdırma
+print("Rastgele seçilen futbol kulübü:", secilen_kulup)
